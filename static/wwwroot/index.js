1a712b5b0cff5f70ab52c7f2e05ca33a5f556c4a
Douman <douman@gmx.se>
2019-03-15 20:06:38 +0300

    Simple display of json data

--- static/wwwroot/index.js
+++ static/wwwroot/index.js
@@ -1,9 +1,42 @@
+function prepare_title_case(str) {
+    return str.replace(/\w\S*/g, function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    })
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-    const JSON_AREA = document.getElementById("json_area")
+    const JSON_AREA = document.getElementById("json_data")
     //Runs when page is loaded
     let API = window.location.protocol + "//" + window.location.host + "/api/http"
 
+    function set_json_data(data) {
+        const nav = JSON_AREA.children[0];
+        const list  = JSON_AREA.children[1];
+
+        for (let idx = 0; idx < data.length; idx++) {
+            let new_link = document.createElement('a')
+            new_link.text = prepare_title_case(data[idx].name)
+            new_link.href = "#" + data[idx].name
+            new_link.className = "nav-link json-data-nav-link"
+
+            nav.appendChild(new_link)
+
+            let new_list = document.createElement('div')
+            new_list.id = data[idx].name
+            new_list.className = "json-data-list target-sub-page"
+            let new_list_text = document.createElement('textarea')
+
+            new_list_text.value = JSON.stringify(data[idx], null, 4)
+            new_list_text.rows = 7
+            new_list_text.cols = 50
+            new_list.appendChild(new_list_text)
+
+
+            list.appendChild(new_list)
+        }
+    }
+
     fetch(API).then((response) => response.json())
-              .then((response) => JSON_AREA.value = JSON.stringify(response, null, 4))
+              .then(set_json_data)
               .catch((error) => JSON_AREA.value = error)
 })
