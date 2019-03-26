33a75b68ccef47abea8dd85a96428846b25c7310
Damian Jarek <damian.jarek93@gmail.com>
2019-03-27 00:39:08 +0100

    CI work:
    - Expand the matrix to include gcc default and gcc-8.
    - Add sanitizer builds.
    - Add coverage collection.
    
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

---
+++ tools/ci_toolchain.cmake
@@ -0,0 +1,26 @@
+mark_as_advanced(FORCE CMAKE_TOOLCHAIN_FILE)
+
+if (BEAST_LOUNGE_CONFIGURED)
+    return()
+endif()
+
+if (DEFINED ENV{BEAST_LOUNGE_SANITIZE})
+    # Enabling sanitizers breaks `Threads::Threads`
+    set(CMAKE_CXX_FLAGS_INIT
+        "${CMAKE_CXX_FLAGS_INIT} -pthread -fuse-ld=gold -fsanitize=$ENV{BEAST_LOUNGE_SANITIZE} -fno-sanitize-recover=$ENV{BEAST_LOUNGE_SANITIZE}")
+    message(STATUS "Enabling sanitizers: $ENV{BEAST_LOUNGE_SANITIZE}")
+endif()
+
+if (DEFINED ENV{BEAST_LOUNGE_BUILD_TYPE})
+    set(CMAKE_BUILD_TYPE
+        $ENV{BEAST_LOUNGE_BUILD_TYPE})
+    message(STATUS "Build type: $ENV{BEAST_LOUNGE_BUILD_TYPE}")
+endif()
+
+if (DEFINED ENV{BEAST_LOUNGE_COVERAGE})
+    set(CMAKE_CXX_FLAGS_INIT
+        "${CMAKE_CXX_FLAGS_INIT} -fprofile-arcs --coverage")
+    message(STATUS "Enabling coverage collection ${CMAKE_CXX_FLAGS}")
+endif()
+
+set(BEAST_LOUNGE_CONFIGURED TRUE)
